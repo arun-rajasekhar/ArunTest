2606401d6334a87400779f1f13fcecc2f8462f83
RamyaSubramaniam <reach2arunraj@gmail.com>
2016-03-15 16:41:44 +0530

    Initial setup

---
+++ Source/ArunTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArunTest
+//
+//  Created by developer1 on 15/03/16.
+//  Copyright © 2016 CTS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
